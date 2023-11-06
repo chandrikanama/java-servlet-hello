ea4ec4a19794b25baa5163adb065d2cab2b7255b
chandrikanama <58563394+chandrikanama@users.noreply.github.com>
2023-11-06 17:33:35 +0530

    Create auto.sh

---
+++ auto.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd ~
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+docker rmi $(docker images -q)
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/g9h5h7f8
+docker build -t demorepo .
+docker tag demorepo:latest public.ecr.aws/g9h5h7f8/demorepo:latest
+docker push public.ecr.aws/g9h5h7f8/demorepo:latest
+docker run -d --name my-container -p 8085:8080 public.ecr.aws/g9h5h7f8/demorepo:latest
